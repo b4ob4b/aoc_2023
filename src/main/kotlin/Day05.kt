68dfb9159b041ff411aacc69a569d6c4dc138428
Julian Baumgartner <julian.baumgaertner@bmw.de>
2023-12-06 07:06:34 +0100

    day5 still brute force, but cleaned up a bit

--- src/main/kotlin/Day05.kt
+++ src/main/kotlin/Day05.kt
@@ -5,86 +5,52 @@ fun main() {
     Day05().solve()
 }
 
-class Day05(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("", inputType = inputType) {
+class Day05(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("If You Give A Seed A Fertilizer", inputType = inputType) {
 
-    val data = input.split("\n\n")
-
-    override fun part1(): Any? {
-        val seeds = data.first().replace("seeds: ", "").split(" ").map { it.toLong() }
-        val maps = data
-            .drop(1)
-            .map { it.splitLines().drop(1) }
-            .map {
-                it.map {
-                    val line = it.split(" ")
-                    val dest = line[0].toLong()
-                    val source = line[1].toLong()
-                    val range = line[2].toLong()
-                    val destmap = dest..(dest + range - 1)
-                    val sourcemap = source..(source + range - 1)
-//                    mapOf(sourcemap to destmap)
-                    destmap to sourcemap
-                }
-            }
-
-
-        val l = maps.fold(seeds) { seeeds, map ->
-
-            val x = seeeds.map { seed ->
-                val m = map.filter { seed in it.second }
-                if (m.isNotEmpty()) {
-                    val delta = m.single().second.first - m.single().first.first
-                    seed.toLong() - delta
-                } else {
-                    seed.toLong()
-                }
-            }
-            x
-        }
-        return l.min()
-
-        return "not yet implement"
-    }
+    private val groups = input.split("\n\n")
+    private val seedNumbers = groups.first().replace("seeds: ", "").split(" ")
 
     private data class Mapper(val source: LongRange, val target: LongRange, val delta: Long)
 
-    override fun part2(): Long {
-        val seeds0 = data.first().replace("seeds: ", "").split(" ").map { it.toLong() }
-        val seeds = seeds0.chunked(2).map { it[0]..(it[0] + it[1] - 1) }
-        val mappersList = data
-            .drop(1)
-            .map { it.splitLines().drop(1) }
-            .map {
-                it.map {
-                    val line = it.split(" ")
-                    val dest = line[0].toLong()
-                    val source = line[1].toLong()
-                    val range = line[2].toLong()
-                    val sourcemap = source..(source + range - 1)
-                    val target = dest..(dest + range - 1)
-                    Mapper(sourcemap, target, dest - source)
-                }
+    private val mappingList = groups
+        .drop(1)
+        .map { it.splitLines().drop(1) }
+        .map { group ->
+            group.map { mapInstruction ->
+                val (dest, source, range) = mapInstruction.split(" ").map { it.toLong() }
+                val sourcemap = source..<source + range
+                val target = dest..<dest + range
+                Mapper(sourcemap, target, dest - source)
             }
+        }
+
+    override fun part1(): Long {
+        val seeds = seedNumbers.map { it.toLong() }
+        return mappingList.fold(seeds) { nextSeeds, map ->
+            nextSeeds.map { seed ->
+                val delta = map.singleOrNull { seed in it.source }?.delta ?: 0
+                seed + delta
+            }
+        }.min()
+    }
+
+    override fun part2(): Long {
+        val seeds = seedNumbers
+            .map { it.toLong() }
+            .chunked(2)
+            .map { (start, range) -> start..<start + range }
+
         var locationCandidate = 0L
         while (true) {
-            val candidate = mappersList.reversed().fold(locationCandidate) { acc, mappers ->
-                val mapper = mappers.filter { acc in it.target }.singleOrNull()
-                if (mapper == null) {
-                    acc
-                } else {
-                    acc - mapper.delta
-                }
+            val candidate = mappingList.reversed().fold(locationCandidate) { acc, mappers ->
+                val delta = mappers.singleOrNull { acc in it.target }?.delta ?: 0
+                acc - delta
             }
             if (seeds.any { seed -> candidate in seed }) {
                 return locationCandidate
             }
             locationCandidate += 1
-            locationCandidate.print()
         }
 
-
-
-
-        return 1
     }
 }
