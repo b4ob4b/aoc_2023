17ff21fca02469830d3b84f150010c01f1f93075
Julian Baumgartner <julian.baumgaertner@bmw.de>
2023-12-09 06:29:45 +0100

    day 9

---
+++ src/main/kotlin/Day09.kt
@@ -0,0 +1,31 @@
+import utils.Day
+import utils.IO
+import utils.extractInts
+import utils.splitLines
+
+fun main() {
+    Day09(IO.TYPE.SAMPLE).test(114, 2)
+    Day09().solve()
+}
+
+class Day09(inputType: IO.TYPE = IO.TYPE.INPUT) : Day("Mirage Maintenance", inputType = inputType) {
+
+    private val sensorData = input.splitLines().map { it.extractInts() }
+
+    override fun part1() = sensorData.sumOf { it.calculateFuture() }
+
+    override fun part2() = sensorData.sumOf { it.calculatePast() }
+
+    private fun List<Int>.calculateFuture(): Int {
+        if (this.all { it == 0 }) return 0
+        val futureValue = this.windowed(2).map { it[1] - it[0] }.calculateFuture()
+        return this.last() + futureValue
+    }
+
+    private fun List<Int>.calculatePast(): Int {
+        if (this.all { it == 0 }) return 0
+        val pastValue = this.windowed(2).map { it[1] - it[0] }.calculatePast()
+        return this.first() - pastValue
+    }
+
+}
